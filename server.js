8853b04302f19b25ae2797a82124bdff7830c7a6
Dillon Cleaver <cleadi@fastmail.com>
2021-11-15 15:24:47 -0600

    Still trying  to fix DB.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budgetDb", {
+mongoose.connect("mongodb://localhost/budget-db", {
   useNewUrlParser: true,
   useFindAndModify: false,
 });
